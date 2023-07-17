1e0e47fa3ab4f48df96e705cc3a3a83e440851ba
Sheriff Amzat <kunmiamzat1@gmail.com>
2023-07-17 20:50:42 +0100

    Initialized File Metadata service

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const fs = require('fs');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const uploadsDir = './uploads';
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
+
+// Set up multer storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Specify the destination folder for uploaded files
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname); // Use the original file name as the file name
+  }
+});
+
+// Create multer upload instance
+const upload = multer({ storage });
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size,
+  })
+})
 
 
 
